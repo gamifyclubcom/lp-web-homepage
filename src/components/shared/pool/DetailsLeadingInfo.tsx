f02103e4789953d49599be241bb7676876605af8
Sotatek-HuyTran3 <huy.tran3@sotatek.com>
2021-12-18 11:31:13 +0700

    fix: update library

--- src/components/shared/pool/DetailsLeadingInfo.tsx
+++ src/components/shared/pool/DetailsLeadingInfo.tsx
@@ -70,9 +70,9 @@ const DetailsLeadingInfo: React.FC<Props> = ({
       </div>
 
       {description && (
-        <span className="max-w-md mb-4 text-sm font-light text-left text-white truncate">
-          {parseDescription()}
-        </span>
+        <div className="max-w-md mb-4 text-sm font-light text-left text-white truncate">
+          <div dangerouslySetInnerHTML={{ __html: parseDescription() }} />
+        </div>
       )}
 
       <ul className="flex justify-start items-left">
