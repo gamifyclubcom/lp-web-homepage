bd0e2f0304fa6243e057f785af3b3d652149a5e5
Sotatek-HuyTran3 <huy.tran3@sotatek.com>
2021-12-22 18:36:47 +0700

    fix: check youtube url

--- src/components/shared/pool/DetailsLeadingInfo.tsx
+++ src/components/shared/pool/DetailsLeadingInfo.tsx
@@ -32,7 +32,7 @@ const DetailsLeadingInfo: React.FC<Props> = ({
   const matchYoutubeUrl = (url: any) => {
     var p =
       /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-    if (url.match(p)) {
+    if (url && url.match(p)) {
       return url.match(p)[1];
     }
     return false;
